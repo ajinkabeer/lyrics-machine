c389e0b9551e63c4d602b37bd208f885b9bb1f82
Ajin Kabeer <ajiinkabeer@gmail.com>
2020-03-15 18:13:29 +0530

    Inject data into dom

--- script.js
+++ script.js
@@ -23,4 +23,21 @@ async function searchSongs(searchTerm) {
 }
 
 //Render songs and artist in DOM
-function showData(data) {}
+function showData(data) {
+  result.innerHTML = `
+  <ul class="songs">
+  ${data.data
+    .map(
+      song =>
+        `
+        <li>
+        <span><strong>${song.artist.name}</strong> - ${song.title}</span>
+        <button class="btn" data-artist="${song.artist.name}" data-songtitle="${song.title}">Get Lyrics</button>
+      </li>
+      
+      `
+    )
+    .join("")}
+  </ul>
+  `;
+}
